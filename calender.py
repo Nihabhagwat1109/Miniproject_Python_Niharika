0ea7099e73d69467bf85a01d7a97623871cdb6ba
Nihabhagwat1109 <136458091+Nihabhagwat1109@users.noreply.github.com>
2023-07-24 18:27:09 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,16 @@
+import calendar
+
+year = int(input("Enter the year: "))
+month = int(input("Enter the month (1-12): "))
+
+print(calendar.month(year, month))
+
+days_in_month = calendar.monthrange(year, month)[1]
+if days_in_month == 30:
+    print(f"The month {calendar.month_name[month]} in {year} has 30 days.")
+elif days_in_month == 31:
+    print(f"The month {calendar.month_name[month]} in {year} has 31 days.")
+else:
+    print(f"The month {calendar.month_name[month]} in {year} has {days_in_month} days.")
+
+print(calendar.calendar(year))
